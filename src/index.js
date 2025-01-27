0a44050b3eb719b2898b2844f01e29cecc59565d
Ali-nasser1 <popa0251@gmail.com>
2025-01-27 16:12:43 +0200

    use redis example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+// connecting with redisDb
+const REDIS_PORT = 6379;
+const REDIS_HOST = 'redis';
+
+const redisClient = redis.createClient({
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+});
+redisClient.on('error', (err) => console.log('Failed to connect to redis', err));
+redisClient.on('connect', () => console.log('connected to redis'));
+redisClient.connect();
+
+// connecting with mongoDb
 const DB_USER = 'root';
 const DB_PASSWORD = 'example';
 const DB_PORT = '27017';
 const DB_HOST = 'mongo';
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 mongoose.connect(URI)
-        .then(() => console.log('DB is connect'))
-        .catch((err) => console.log('Failed to connect to db', err));
+        .then(() => console.log('mongoDb is connect'))
+        .catch((err) => console.log('Failed to connect to mongoDb', err));
 
-app.get('/', (req, res) => res.send('<h1> Hello Ali Nasser, swe</h1>'))
+// enpoints
+app.get('/', (req, res) => {
+    redisClient.set("Cached", "Yes Cached...");
+    res.send('<h1> Hello Ali Nasser, swe</h1>');
+});
+
+app.get('/data', async (req, res) => {
+    const data = await redisClient.get("Cached");
+    res.send(`<h1> Hello Ali Nasser </h1> <h2> ${data} </h2>`);
+});
 app.listen(PORT, () => console.log('app is running'));
