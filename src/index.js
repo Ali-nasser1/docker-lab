a027698459679c0d4cb3d312883e57b8f1461ff3
Ali-nasser1 <popa0251@gmail.com>
2025-01-27 14:36:47 +0200

    install mongoDb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = '27017';
+const DB_HOST = 'mongo';
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose.connect(URI)
+        .then(() => console.log('DB is connect'))
+        .catch((err) => console.log('Failed to connect to db', err));
 
 app.get('/', (req, res) => res.send('<h1> Hello Ali Nasser, swe</h1>'))
 app.listen(PORT, () => console.log('app is running'));
